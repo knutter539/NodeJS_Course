0a84f23edf0943a62318d4c9576d21cc459338e6
Nick Baranowski <njbarano@gmail.com>
2019-02-21 15:07:14 -0500

    Ignore node-module

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -6,11 +6,19 @@ const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
 
-//console.log('Hello ' + user.username);
+////lodash isString()
+//onsole.log(_.isString(true));
+//console.log(_.isString('TittySprinkles'));
+
+//lodash _.uniq()
+var filteredArray = _.uniq([2,1,2,4,4,'Nick','Nick']);
+console.log(filteredArray);
+
 
 // var res = notes.addNote();
 // console.log(res);
 
+////use os to get username and update log file.
 // var user = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age} years old!`, function (err) {
 //     if (err) { 
@@ -18,5 +26,6 @@ const _ = require('lodash');
 //     }
 // });
 
-console.log(notes.add(10,15));
+////addition function from notes module
+//console.log(notes.add(10,15));
 
